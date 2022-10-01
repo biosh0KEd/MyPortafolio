dca81b3a82a433314659f69d0f9f9866a5f85de4
Efraín Girón Romero <efrainbond@hotmail.com>
2022-10-01 16:38:10 -0500

    Cambio de urls para release.

---
+++ Portafolio/Data/Utils.cs
@@ -0,0 +1,14 @@
+﻿namespace Portafolio.Data
+{
+    public class Utils
+    {
+        public static string getURL(string url)
+        {
+#if DEBUG
+            return url;
+#else
+            return $"/MyPortafolio{url}";
+#endif
+        }
+    }
+}
